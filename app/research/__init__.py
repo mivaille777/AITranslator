d1c11c2ccd6f11fa4eee46a20416d8857b734f82
mivaille777 <2735545778@qq.com>
2026-08-20 03:46:07 +0800

    Add research notes package

---
+++ app/research/__init__.py
@@ -0,0 +1,15 @@
+"""Persistent research-memory primitives for AITranslator."""
+
+from app.research.notes import (
+    DEFAULT_RESEARCH_NOTES_FILENAME,
+    ResearchNote,
+    ResearchNoteSaveResult,
+    ResearchNoteStore,
+)
+
+__all__ = [
+    "DEFAULT_RESEARCH_NOTES_FILENAME",
+    "ResearchNote",
+    "ResearchNoteSaveResult",
+    "ResearchNoteStore",
+]

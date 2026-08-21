d765eb3f51b88412c33a4d8dad264521dbed779f
mivaille777 <2735545778@qq.com>
2026-08-21 15:47:42 +0800

    Export Stage 6C reading adapters

--- app/selection/__init__.py
+++ app/selection/__init__.py
@@ -1,21 +1,31 @@
 """Selection-layer components."""
 
-from app.models.selection import SelectedText
+from app.models.selection import DocumentIdentity, ReadingSelection, SelectedText
 from app.selection.base import SelectionProvider
 from app.selection.clipboard_provider import ClipboardSelectionProvider
 from app.selection.errors import SelectionError
 from app.selection.foreground import ForegroundApplicationDetector
 from app.selection.manager import SelectionManager
+from app.selection.reading_context import (
+    browser_snapshot_to_reading_selection,
+    reading_selection_from_selected_text,
+    reading_selection_to_context,
+)
 from app.selection.uia_provider import UIASelectionProvider
 from app.selection.word_provider import WordSelectionProvider
 
 __all__ = [
     "ClipboardSelectionProvider",
+    "DocumentIdentity",
+    "ForegroundApplicationDetector",
+    "ReadingSelection",
     "SelectedText",
     "SelectionError",
-    "ForegroundApplicationDetector",
     "SelectionManager",
     "SelectionProvider",
     "UIASelectionProvider",
     "WordSelectionProvider",
+    "browser_snapshot_to_reading_selection",
+    "reading_selection_from_selected_text",
+    "reading_selection_to_context",
 ]
